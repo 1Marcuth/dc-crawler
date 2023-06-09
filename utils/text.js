230d79f454aec558119fb244a700e8dfeaf982dc
TantoFaz <thiago.playgames15@gmail.com>
2023-06-09 10:23:04 -0300

    correções

--- utils/text.js
+++ utils/text.js
@@ -1,5 +1,10 @@
 function toTitleCase(str) {
-    return str.toLowerCase().replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase())
+    return str
+        .toLowerCase()
+        .replace(
+            /(^\w{1})|(\s+\w{1})/g,
+            letter => letter.toUpperCase()
+        )
 }
 
 export { toTitleCase }
